0296513bfdd3f531506e98afb0936bec25993dd3
jcarlosmariscal <juanc.ramirezm16@gmail.com>
2022-09-06 20:28:39 -0500

    App de introducción

---
+++ PrimerCorte/10A_DMI/01-fundamentos/src/App.vue
@@ -0,0 +1,32 @@
+<!-- VUE.js SFC - Single File Component 
+  JS-script 
+  CSS-style 
+  HTML-template 
+  contador: 0; 
+  contador++;
+
+<script></script> -->
+
+<template>
+  <div class="alert alert-success" role="alert">
+    <h4 class="alert-heading">Juan Carlos Ramírez Mariscal - DGS - 10A</h4>
+    <p>App de Introducción</p>
+    <hr />
+    <p class="mb-0">06 de septiembre de 2022</p>
+  </div>
+  <br />
+  <img src="./assets/logo.svg" alt="Logo Vue" width="125" height="125" /><br />
+  <h1>Hola desde VUE.js 3</h1>
+  <br />
+  <h2>Contador 0</h2>
+  <br />
+  <button>+1</button>
+  <button>-1</button>
+  <button>Reset</button>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
