32443b00e283a3bde646718fb2245bed4ed1d1a6
jcarlosmariscal <juanc.ramirezm16@gmail.com>
2022-09-10 15:46:14 -0500

    Subir 01-fundamentos a la carpeta general

--- PrimerCorte/10A_DMI/01-fundamentos/src/App.vue
+++ PrimerCorte/10A_DMI/01-fundamentos/src/App.vue
@@ -6,27 +6,99 @@
   contador++;
 
 <script></script> -->
+<script>
+// export default {
+//   data(){
+//     return{
+//       contador: 200,
+//     };
+//   },
+// };
+export default {
+  data: () => ({
+    contador: 0,
+  }),
+  methods: {
+    saludar() {
+      console.log("Hola 10A");
+    },
+    incrementar() {
+      this.contador++;
+    },
+    decrementar() {
+      this.contador--;
+    },
+    resetear() {
+      this.contador = 0;
+    },
+  },
+};
+</script>
 
 <template>
-  <div class="alert alert-success" role="alert">
-    <h4 class="alert-heading">Juan Carlos Ramírez Mariscal - DGS - 10A</h4>
-    <p>App de Introducción</p>
-    <hr />
-    <p class="mb-0">06 de septiembre de 2022</p>
+  <img src="./assets/logo.svg" alt="Logo Vue" width="125" height="125" />
+
+  <div class="container">
+    <small>
+      <h1>Hola desde VUE.js 3</h1>
+    </small>
+    <br />
+    <h2>Contador {{ contador }}</h2>
+    <br />
+    <button @click="incrementar()">+1</button>
+    <button @click="decrementar()">-1</button>
+    <button @click="resetear()">Reset</button>
+    <!-- Button trigger modal -->
+    <button
+      type="button"
+      class="btn-m"
+      data-toggle="modal"
+      data-target="#modal"
+    >
+      Open
+    </button>
   </div>
-  <br />
-  <img src="./assets/logo.svg" alt="Logo Vue" width="125" height="125" /><br />
-  <h1>Hola desde VUE.js 3</h1>
-  <br />
-  <h2>Contador 0</h2>
-  <br />
-  <button>+1</button>
-  <button>-1</button>
-  <button>Reset</button>
+  <!-- Modal -->
+  <div class="modal fade" id="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">
+            Juan Carlos Ramírez Mariscal - DGS - 10A
+          </h5>
+        </div>
+        <div class="modal-body">
+          <span>VUE Deploy: Netlify, Vercel y GitHub</span>
+          <span>Clase 9 de septiembre de 2022</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.container {
+  text-align: center;
+}
+small {
+  color: #666;
+}
 h1 {
-  color: red;
+  font-weight: bolder;
+}
+h2 {
+  font-size: 2.5rem;
+}
+button {
+  cursor: pointer;
+  font-size: 1.5rem;
+  margin: 0px 8px;
+}
+
+h5 {
+  color: #34495e;
+}
+span {
+  color: #34495e;
 }
 </style>
