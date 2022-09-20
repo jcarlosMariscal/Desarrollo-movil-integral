26216bcff22ec9e0a9c55935cded780aea14ade4
jcarlosmariscal <juanc.ramirezm16@gmail.com>
2022-09-19 22:50:48 -0500

    Clase 19-09-2022

---
+++ PrimerCorte/10A_DMI/02-componentes/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Componentes en VUE</h1>
+  <hr />
+  <card url="https://cdn.svgporn.com/logos/vue.svg" nombre="Vue.js" />
+  <card url="https://cdn.svgporn.com/logos/angular-icon.svg" nombre="Angular" />
+  <card url="https://cdn.svgporn.com/logos/vuetifyjs.svg" nombre="Vuetify" />
+  <card url="https://cdn.svgporn.com/logos/react.svg" nombre="React" />
+</template>
+<script>
+import Card from "./components/Card.vue";
+export default {
+  components: { Card },
+};
+</script>
---
+++ PrimerCorte/10A_DMI/02-componentes/src/components/Card.vue
@@ -0,0 +1,37 @@
+<template>
+  <grid>
+    <img v-bind:src="url" :alt="nombre" />
+    <h2>{{ nombre }}</h2>
+    <a :href="url" target="_blank">Descargar</a>
+    <a :href="url" target="_blank">Visitar</a>
+  </grid>
+</template>
+<script>
+import Grid from "./Grid.vue";
+export default {
+  props: {
+    url: {
+      String,
+    },
+    nombre: {
+      String,
+    },
+  },
+  components: { Grid },
+};
+</script>
+<style scoped>
+img {
+  width: 8rem;
+  padding: 8rem;
+  background: snow;
+}
+a {
+  text-decoration: none;
+  color: snow;
+  background-color: #301675;
+  padding: 0.5rem;
+  border-radius: 5px;
+  border: 1px solid #f2f2f2;
+}
+</style>
