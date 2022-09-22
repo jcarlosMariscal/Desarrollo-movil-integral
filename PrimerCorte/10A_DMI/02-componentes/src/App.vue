30cb1ac6592d09efd1f1b65b4043fc22672dffd1
jcarlosmariscal <juanc.ramirezm16@gmail.com>
2022-09-21 23:41:51 -0500

    Videos clase 19-09-22

--- PrimerCorte/10A_DMI/02-componentes/src/App.vue
+++ PrimerCorte/10A_DMI/02-componentes/src/App.vue
@@ -5,10 +5,19 @@
   <card url="https://cdn.svgporn.com/logos/angular-icon.svg" nombre="Angular" />
   <card url="https://cdn.svgporn.com/logos/vuetifyjs.svg" nombre="Vuetify" />
   <card url="https://cdn.svgporn.com/logos/react.svg" nombre="React" />
+  <button
+    type="button"
+    class="btn btn-primary"
+    data-toggle="modal"
+    data-target="#modal"
+  >
+    Modal
+  </button>
 </template>
 <script>
 import Card from "./components/Card.vue";
+import Slot from "./components/Slot.vue";
 export default {
-  components: { Card },
+  components: { Card, Slot },
 };
 </script>
